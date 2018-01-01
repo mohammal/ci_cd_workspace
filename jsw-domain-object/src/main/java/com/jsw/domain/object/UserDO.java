3e7d533e8d219f958fbf4f4a28ddcf1a2dc6be9c
https://code.in.spdigital.io/ <rajesh.b.yadav@capgemini.com>
2018-01-01 18:47:58 +0530

    added dependent modules

---
+++ jsw-domain-object/src/main/java/com/jsw/domain/object/UserDO.java
@@ -0,0 +1,11 @@
+package com.jsw.domain.object;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name="/jsw-domain-object/src/main/java/com/jsw/domain/object/UserDO")
+@Table(name="users")
+public class UserDO {
+	
+
+}
